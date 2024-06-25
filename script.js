2b9cdfdbb56d9ade6b839f6075e5767bf5f5e6d9
sardor <alimukhammad1@gmail.com>
2024-06-24 23:49:41 -0400

    2

--- script.js
+++ script.js
@@ -22,35 +22,35 @@ $(document).ready(function () {
 
 	$(".next-step").click(function () { //when next button is clicked, .next-step is a class of button
 
-		currentGfgStep = $(this).parent(); // get the parent of button clicked, chi
-		nextGfgStep = $(this).parent().next(); 
+		currentGfgStep = $(this).parent(); // get the parent of button clicked, check the html code, this is the fieldset
+		nextGfgStep = $(this).parent().next(); // get the next parent of button clicked, this is the next fieldset because we are in fieldset
 
-		$("#progressbar li").eq($("fieldset").index(nextGfgStep)).addClass("active"); 
+		$("#progressbar li").eq($("fieldset").index(nextGfgStep)).addClass("active"); // add active class to the next fieldset
 
-		nextGfgStep.show(); 
-		currentGfgStep.animate({ opacity: 0 }, { 
-			step: function (now) { 
-				opacity = 1 - now; 
+		nextGfgStep.show(); 				// show the next fieldset
+		currentGfgStep.animate({ opacity: 0 }, { // animate the current fieldset to opacity 0 because we are going to hide it
+			step: function (now) { // step function is called for each step in the animation, now is the current value of the animation
+				opacity = 1 - now; // opacity is 1 - now, so it will decrease from 1 to 0
 
-				currentGfgStep.css({ 
+				currentGfgStep.css({ // css for current fieldset
 					'display': 'none', 
 					'position': 'relative'
 				}); 
-				nextGfgStep.css({ 'opacity': opacity }); 
+				nextGfgStep.css({ 'opacity': opacity }); // css for next fieldset, opacity will increase from 0 to 1
 			}, 
-			duration: 500 
+			duration: 500 // duration of animation between current and next fieldset
 		}); 
-		setProgressBar(++current); 
+		setProgressBar(++current); // call progress bar function and increase the current step
 	}); 
 
-	$(".previous-step").click(function () { 
+	$(".previous-step").click(function () { // when previous button is clicked, .previous-step is a class of button
 
-		currentGfgStep = $(this).parent(); 
-		previousGfgStep = $(this).parent().prev(); 
+		currentGfgStep = $(this).parent(); // get the parent of button clicked, check the html code, this is the fieldset
+		previousGfgStep = $(this).parent().prev(); // get the previous parent of button clicked, this is the previous fieldset because we are in fieldset
 
-		$("#progressbar li").eq($("fieldset").index(currentGfgStep)).removeClass("active"); 
+		$("#progressbar li").eq($("fieldset").index(currentGfgStep)).removeClass("active"); // remove active class from the current fieldset
 
-		previousGfgStep.show(); 
+		previousGfgStep.show(); // show the previous fieldset
 
 		currentGfgStep.animate({ opacity: 0 }, { 
 			step: function (now) { 
@@ -67,13 +67,13 @@ $(document).ready(function () {
 		setProgressBar(--current); 
 	}); 
 
-	function setProgressBar(currentStep) { 
-		var percent = parseFloat(100 / steps) * current; 
-		percent = percent.toFixed(); 
-		$(".progress-bar").css("width", percent + "%") 
+	function setProgressBar(currentStep) { // this function is used to set the progress bar according to the current step
+		var percent = parseFloat(100 / steps) * current; // calculate the percent according to the current step
+		percent = percent.toFixed(); // round off the decimal value
+		$(".progress-bar").css("width", percent + "%") // set the width of the progress bar
 	} 
 
-	$(".submit").click(function () { 
+	$(".submit").click(function () { // when the submit button is clicked
 		return false; 
 	}) 
 }); 
